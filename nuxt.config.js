cb4900467ba426f51e68e10406ebc331e21cdd9d
Leigh R. Dinaya <leighdinaya04@gmail.com>
2020-05-25 11:00:27 +0800

    generate routes

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,6 +45,14 @@ export default {
    ** Nuxt.js modules
    */
   modules: ["@nuxt/content"],
+  generate: {
+    async routes () {
+      const { $content } = require('@nuxt/content')
+      const files = await $content().only(['path']).fetch()
+
+      return files.map(file => file.path === '/index' ? '/' : file.path)
+    }
+  },
   /*
    ** Build configuration
    */
